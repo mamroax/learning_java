a25816d7ee122ec09310438feca0bb86dccc5bc4
mamroax <lefrutite@gmail.com>
2023-02-10 16:42:06 +0300

    added third simple class

---
+++ project1/src/ConsoleOutputDemo.java
@@ -0,0 +1,8 @@
+// Описание класса ConsoleOutputDemo
+public class ConsoleOutputDemo {
+    // Главный метод программы:
+    public static void main(String[] args){
+        // Вывод сообщения в консольное окно (окно вывода):
+        System.out.println("Мы изучаем язык Java!");
+    }
+}
