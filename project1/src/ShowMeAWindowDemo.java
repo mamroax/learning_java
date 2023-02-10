d808a0798163d1dfa6e00942e646ffc7afeb6ccd
mamroax <lefrutite@gmail.com>
2023-02-10 16:06:24 +0300

    +

---
+++ project1/src/ShowMeAWindowDemo.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+public class ShowMeAWindowDemo {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Первая программа на Java!");
+    }
+}
